fc2806a121a44f1190f5aeaec0859f53795e97c3
Chris Maxwell <chris@wrathofchris.com>
2023-06-26 22:27:53 -0700

    for public record, eventually I will learn to auto-run tests AND typecheck before commit

--- tests/version.py
+++ tests/version.py
@@ -4,6 +4,6 @@ from incline.version import incline_version
 
 class TestVersion(unittest.TestCase):
 
-    def test_version(self):
+    def test_version(self) -> None:
         version = incline_version()
         self.assertIsNotNone(version)
